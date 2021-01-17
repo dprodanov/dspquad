7090d5e03127b57cae68cce76c71ee454ba5ed03
prodanov <prodanov@ICT0120821.imec.be>
2021-01-17 14:42:40 +0100

    Wynn epsilon algorithm for convergence acceleration

---
+++ dspquad/src/test/WynnTest.java
@@ -0,0 +1,38 @@
+package test;
+
+import static ijaux.quad.QuadBess.*;
+
+public class WynnTest {
+
+	public WynnTest() {
+ 
+	}
+	
+public static void main(String[] args) {
+		
+		System.out.println("Arc tan series for PI/4");
+		 double[] a=new double[] 
+				 {1,-0.3333333333333333,0.2,-0.1428571428571428,0.1111111111111111,-0.09090909090909091,0.07692307692307693};
+		 double[] ac= cumsum(a);
+		 
+		 for (int i=0; i<ac.length; i++)
+			 System.out.print(ac[i]+" ");
+		 System.out.println();
+		 System.out.println("----------------------------");
+		 double[][] warr= wynn(ac);
+		
+		 for (int i=0; i<warr.length; i++) {
+			 for (int j=0; j<warr[i].length; j++) {
+				 System.out.print(warr[i][j]+" | ");
+			 }
+			 System.out.println("\r\n----------------------------");
+		 }
+		 int n=warr.length-1;
+		 int m=warr[n].length-1;
+		 System.out.println("4*W  -PI");
+		 System.out.println(warr[n][m]*4.0- Math.PI );
+		 System.out.println("W  - atan(1)");
+		 System.out.println(warr[n][m]- Math.atan(1.0) );
+	}
+
+}
---
+++ dspquad/src/test/unit/WynnTest1.java
@@ -0,0 +1,25 @@
+package test.unit;
+
+import static org.junit.jupiter.api.Assertions.*;
+import static ijaux.quad.QuadBess.waccelerate;
+
+import ijaux.quad.Utils;
+
+import org.junit.jupiter.api.Test;
+
+class WynnTest1 {
+
+	final double tol=1e-5;
+	
+	@Test
+	void testWaccelerate() {
+		 double[] a=new double[] 
+				 {1,-0.3333333333333333,0.2,-0.1428571428571428,0.1111111111111111,-0.09090909090909091,0.07692307692307693};
+		 double ret=waccelerate(a);
+		 double at1= Math.atan(1.0);
+		 System.out.println(ret- at1);
+		 assert( Utils.apperoxeq(ret, at1, tol ));
+		//fail("Not yet implemented");
+	}
+
+}
