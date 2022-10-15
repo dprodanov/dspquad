8f6306f74424af53a3275c39afefdf0ebf2ac2a6
prodanov <prodanov@ICT0120821.imec.be>
2022-10-16 01:37:13 +0200

    Prabhakar function

--- dspquad/src/ijaux/quad/misc/MiLePW.java
+++ dspquad/src/ijaux/quad/misc/MiLePW.java
@@ -3,29 +3,32 @@ package ijaux.quad.misc;
 import static ijaux.quad.QuadDE.intdei;
 
 import ijaux.quad.QFunction;
-//import ijaux.quad.euler.Gamma;
+import ijaux.quad.euler.Gamma;
 import ijaux.quad.wright.GMWright;
 
-public class MiLeW implements QFunction {
+public class MiLePW implements QFunction {
 	
 	GMWright gmw;
 	private double a;
 	private double b;
+	private double g;
 	
-	//Gamma gam=new Gamma();
+	private Gamma gam=new Gamma();
 	
 	////////////////////
 	private class KerF implements QFunction {
 	 
 		double z=0;
+		double g=0;
 		
-		public void setVal( double  zz) {
+		public void setVal(double gg, double  zz) {
 			z=(zz);
+			g=gg;
 		}
 		
 		@Override
 		public double eval(double u) {
-			final double bk=gmw.eval(u*z);
+			final double bk=gmw.eval(u*z)*Math.pow(u, g-1.);
 			return Math.exp(-u)*bk;
 		}
 		
@@ -35,7 +38,7 @@ public class MiLeW implements QFunction {
 		
 		@Override
 		public String toString() {
-			return "exp(-u)* W(a,b, zu)"; 
+			return "exp(-u)* u^(g-1)* W(a,b, zu)"; 
 		}
 	}
 	//////////////////
@@ -43,28 +46,30 @@ public class MiLeW implements QFunction {
 	private KerF qf=new KerF();
 	private double tol=1.0e-15;
 	
+	
 	double fr=1.;
 	/**
 	 * 
 	 */
-	public MiLeW(double aa, double bb) {
+	public MiLePW(double aa, double bb, double gg) {
 		a=aa;
 		b=bb;
+		g=gg;
 		gmw= new GMWright(a,b);
-		//fr=1/gam.eval(b);
+		fr=1/gam.eval(g);
 
 	}
 
 	@Override
 	public double eval(double x) {
 		double ret=qf.prefactor();
-		qf.setVal(x);
+		qf.setVal(g, x);
 		final double[] vv=intdei(qf, 0.0, tol);
  		return vv[0]*ret;
 	}
 
 	public static void main(String[] args) {
-		MiLeW g= new MiLeW(1.0, 1.0);
+		MiLePW g= new MiLePW(1.0, 1.0, 1.0);
 		
 		double ret=g.eval(0);
 		System.out.println ("E(1, 1 |0) "+ ret);
@@ -75,15 +80,15 @@ public class MiLeW implements QFunction {
 		System.out.println ("E(1, 1 |5) "+ ret+ " "+ Math.exp(x));
 		
 		// (exp(x)-1)/x
-		g=new MiLeW(1.0, 2.0);
+		g=new MiLePW(1.0, 2.0, 0.5);
 
 		ret=g.eval(x);
 		System.out.println ("E(1, 2 |5) "+ ret+ " "+ (Math.exp(x)-1.)/x);
 		
-		g=new MiLeW(2.0, 3.0);
+	//	g=new MiLePW(2.0, 3.0);
 
-		ret=g.eval(x);
-		System.out.println ("E(1, 2 |5) "+ ret+ " "+ (Math.exp(Math.sqrt(x))/(2.*x)+Math.exp(-Math.sqrt(x))/(2.*x)-1./x ));
+	//	ret=g.eval(x);
+	//	System.out.println ("E(1, 2 |5) "+ ret+ " "+ (Math.exp(Math.sqrt(x))/(2.*x)+Math.exp(-Math.sqrt(x))/(2.*x)-1./x ));
 		
 		
 		
