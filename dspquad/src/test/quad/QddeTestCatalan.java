3b98dfc352d1172d2223306229fa6820ace65dac
Dimiter Prodanov <prodanov@453YTV3.imec.be>
2024-03-04 23:14:19 +0200

    Added more tests

--- dspquad/src/test/quad/QddeTestCatalan.java
+++ dspquad/src/test/quad/QddeTestCatalan.java
@@ -6,9 +6,7 @@ import static ijaux.quad.QuadDE.*;
 import ijaux.quad.QFunction;
  
  
-
-
-//Euler Gamma constant
+ 
 class CatlanAsin implements QFunction {
 	@Override
 	public double eval(double x) {
@@ -37,6 +35,19 @@ class CatlanAtan implements QFunction {
 	}
 }
 
+class CatlanLog implements QFunction {
+	@Override
+	public double eval(double x) {
+		double y=log(x);
+		if (y==0.0) return 0.0;
+		return y/(x*x+1);
+	}
+
+	@Override
+	public String toString() {
+		return "log(x)/(x^2+1)";
+	}
+}
  
 
 public class QddeTestCatalan {
@@ -54,9 +65,23 @@ public static void main(String[] args) {
 		   System.out.println (" I_5="+ ret[0]*0.5+"\t, err= "+ret[1]+ "\t, "+0.9159655941772190150546035);
 		   
 		   System.out.println("\nI_5= \\int_0^\\1 atan(x)/x dx\n");
+		   
 		   CatlanAtan fg1=new CatlanAtan();
 		   
-		    ret=intdeaf(fg1, 0.0, 1.0, 1.0e-15);
+		   ret=intdeaf(fg1, 0.0, 1.0, 1.0e-15);
 		   System.out.println (" I_5="+ ret[0]+"\t, err= "+ret[1]+ "\t, "+ 0.9159655941772190150546035);
+		   
+		   ret=intdea(fg1, 0.0, 1.0, 1.0e-15);
+		   System.out.println (" I_5="+ ret[0]+"\t, err= "+ret[1]+ "\t, "+ 0.9159655941772190150546035);
+		   
+		   ret=intde(fg1, 0.0, 1.0, 1.0e-15);
+		   System.out.println (" I_5="+ ret[0]+"\t, err= "+ret[1]+ "\t, "+ 0.9159655941772190150546035);
+		  
+			System.out.println("\nI_5= \\int_1^\\infty  log(x)/( x^2+1) dx\n");
+		   CatlanLog fg2=new CatlanLog();
+		   
+		   ret=intdei(fg2, 1.0, 1.0e-15);
+		   System.out.println (" I_5="+ ret[0]+"\t, err= "+ret[1]+ "\t, "+ 0.9159655941772190150546035);
+		   
 	}
 }
